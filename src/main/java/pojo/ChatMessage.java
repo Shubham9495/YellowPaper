dcbf276f15a2b03431dfdbd1b649c49121457d6a
Shubham <das.shubham011@gmail.com>
2018-05-14 00:01:15 +0530

    First Commit

---
+++ src/main/java/pojo/ChatMessage.java
@@ -0,0 +1,28 @@
+package pojo;
+
+import java.io.*;
+
+public class ChatMessage implements Serializable {
+
+//    protected static final long serialVersionUID = 1112122200L;
+    public static final int WHOISIN = 0, MESSAGE = 1, LOGOUT = 2;
+    private int type;
+    private String message;
+
+
+    // constructor
+    public ChatMessage(int type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+    // getters
+    public int getType() {
+        return type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
